0af7713c4e3ec1feb29be6b02d69f0444f8eb259
Joshua Shinkle <joshuashinkle@beta.campus.tayloru.edu>
2020-09-29 17:55:14 -0400

    test code for BitFlip

--- TUSatellite7.c
+++ TUSatellite7.c
@@ -2,15 +2,28 @@
   Blank Simple Project.c
   http://learn.parallax.com/propeller-c-tutorials 
 */
-#include "simpletools.h"                      // Include simple tools
+#include "simpletools.h"
+#include "BitFlip.h"
+                      // Include simple tools
+
+const uint16_t addr = 32768;
+uint8_t data = 0;
+uint8_t output;
 
 int main()                                    // Main function
 {
   // Add startup code here.
-
- 
+  
   while(1)
   {
+    setEepromBytes(addr, &data, 1);
+    
+    readEepromBytes(addr, &output, 1);
+    
+    printf("%i\n", output);
+    
+    break;
+    
     // Add main loop code here.
     
   }  
