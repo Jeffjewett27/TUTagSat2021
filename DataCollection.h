e45bc95f3c597cdf78d244cb6c0f02beba58d52b
Jeff <jeffjewett27@gmail.com>
2020-10-06 17:02:43 -0400

    comments

--- DataCollection.h
+++ DataCollection.h
@@ -1,12 +1,12 @@
 void minuteLoop();
-void subscribeInitialize(void(*func)());
-void subscribeEveryTwoSec(void(*func)(int i));
-void subscribeEveryFourSec(void(*func)(int i));
-void subscribeEveryEightSec(void(*func)(int i));
-void subscribeCleanup(void(*func)());
+void subscribeInitialize(void(*func)()); //initializes each minute
+void subscribeEveryTwoSec(void(*func)(int i)); //use for 1 byte values
+void subscribeEveryFourSec(void(*func)(int i)); //use for 2 byte values
+void subscribeEveryEightSec(void(*func)(int i)); //use for 4 byte values
+void subscribeCleanup(void(*func)()); //called after minute is up
 
-void subscribeEveryTwoSecAfter(void(*func)(int i));
-void subscribeEveryFourSecAfter(void(*func)(int i));
+void subscribeEveryTwoSecAfter(void(*func)(int i)); //for pulse counts
+void subscribeEveryFourSecAfter(void(*func)(int i)); //for pulse counts
 
 void callEachAction(void(**actions)(), int num);
 void callEachSubscriber(void(**subscribers)(int iter), int num, int val, uint16_t mask);
